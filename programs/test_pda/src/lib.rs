79b72fae3fe76a9d9d0e4bd9392374ca35cd16d8
lawmeskiviahs <shaiviksemwal@gmail.com>
2022-05-17 14:37:32 +0530

    transfer

--- programs/test_pda/src/lib.rs
+++ programs/test_pda/src/lib.rs
@@ -1,6 +1,8 @@
 use anchor_lang::prelude::*;
 use solana_program::system_program;
 use solana_program::entrypoint::ProgramResult;
+use solana_program::program::invoke;
+use solana_program::system_instruction;
 declare_id!("9fnWiZpicj8MNHymzYiKpA9GtdxqKeV5p7AYevp9hzWF");
 
 #[program]
@@ -8,20 +10,58 @@ pub mod myprogram {
     use super::*;
 
  pub fn initialize(ctx: Context<Initialize>, _bump:u8) -> ProgramResult {
-     msg!("Welcome to pda creation");
+    msg!("Welcome to pda creation");
         let base_account: &mut Account<BaseAccount> = &mut ctx.accounts.base_account;
         base_account.counter += 1;
         base_account.key = base_account.key();
         msg!("base_account.counter = {}", base_account.counter);
-        return Ok(());
+    return Ok(());
     }
 
     pub fn fetch(ctx: Context<FetchInfo>, _bump:u8) -> ProgramResult {
         msg!("Welcome to pda fetch data");
            let base_account: &mut Account<BaseAccount> = &mut ctx.accounts.base_account;
            msg!("base_account.counter = {}", base_account.counter);
-           return Ok(());
-       }
+        return Ok(());
+    }
+
+    pub fn transfer_nft(ctx: Context<TransferNFT>) -> ProgramResult {
+        msg!("Welcome to pda transfer lamports");
+        
+            let amount = 1;
+
+            let ix = spl_token::instruction::transfer(
+                ctx.accounts.token_program.key,
+                ctx.accounts.from_token_account.key,
+                ctx.accounts.to_token_account.key,
+                ctx.accounts.from_account.key,
+                &[ctx.accounts.from_account.key],
+                amount,
+            )?;
+            invoke(
+                &ix,
+                &[
+                    ctx.accounts.from_token_account.clone(),
+                    ctx.accounts.to_token_account.clone(),
+                    ctx.accounts.from_account.clone(),
+                    ctx.accounts.token_program.clone(),
+                ],
+            )?;
+
+        return Ok(());
+    }
+
+    pub fn transfer_lamports(ctx: Context<TransferSOL>) -> ProgramResult {
+        
+        let amount = 10000000;
+
+        invoke(
+            &system_instruction::transfer(ctx.accounts.from_account.key, ctx.accounts.to_account.key, amount),
+            &[ctx.accounts.from_account.clone(), ctx.accounts.to_account.clone()],
+        )?;
+        return Ok(());
+    }
+
 }
 
 #[derive(Accounts)]
@@ -53,6 +93,26 @@ pub struct FetchInfo<'info> {
             ], 
         bump)]
     pub base_account: Account<'info, BaseAccount>,
+}
+#[derive(Accounts)]
+pub struct TransferNFT<'info> {
+    #[account(mut)]
+    pub from_account: AccountInfo<'info>,
+    #[account(mut)]
+    pub from_token_account: AccountInfo<'info>,
+    #[account(mut)]
+    pub to_token_account: AccountInfo<'info>,
+    #[account(mut)]
+    pub token_program: AccountInfo<'info>,
+}
+
+#[derive(Accounts)]
+pub struct TransferSOL<'info> {
+    #[account(mut)]
+    pub from_account: AccountInfo<'info>,
+    pub to_account: AccountInfo<'info>,
+    #[account(mut)]
+    pub system_program: AccountInfo<'info>,
 }
 
 #[account]
